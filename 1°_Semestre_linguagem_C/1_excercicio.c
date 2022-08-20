272626e114c8a135270fee1d2c85eac7a47482dd
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2022-08-20 00:21:09 -0300

    Update 1_excercicio.c

--- 1°_Semestre_linguagem_C/1_excercicio.c
+++ 1°_Semestre_linguagem_C/1_excercicio.c
@@ -4,8 +4,8 @@
 int main(){
     int num=0, peso=0;
     int cont;
+    printf("Entre com 15 números e o sistema selecionará o maior digitado\n");
     for(cont=1;cont<=15;cont++){
-    	printf("Entre com 15 números e o sistema selecionará o maior digitado");
         printf("\n%d)Digite um número: ", cont);
         scanf("%d",&num);
         peso=(num>peso?num:peso);
