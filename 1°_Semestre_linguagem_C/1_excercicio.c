ee2a2fbc745db04f11220d6ce61faff3b4e6f355
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2022-08-20 00:49:46 -0300

    Update 1_excercicio.c

--- 1°_Semestre_linguagem_C/1_excercicio.c
+++ 1°_Semestre_linguagem_C/1_excercicio.c
@@ -1,10 +1,12 @@
+//1-Desenvolva um programa que leia 15 números inteiros e positivos e mostre o maior deles.
+
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(){
     int num=0, peso=0;
     int cont;
-    printf("Entre com 15 números e o sistema selecionará o maior digitado\n");
+    printf("Entre com 15 números e o sistema selecionará o maior número digitado\n");
     for(cont=1;cont<=15;cont++){
         printf("\n%d)Digite um número: ", cont);
         scanf("%d",&num);
