23db865c22487421a4e9152a78d1ef3507a2b329
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2022-08-19 23:49:27 -0300

    Ajuste cod exercicio

--- 1°_Semestre_linguagem_C/1_excercicio.c
+++ 1°_Semestre_linguagem_C/1_excercicio.c
@@ -2,10 +2,10 @@
 #include<stdlib.h>
 
 int main(){
-    int num, peso;
+    int num=0, peso=0;
     int cont;
-    num=peso=0;
     for(cont=1;cont<=15;cont++){
+    	printf("Entre com 15 números e o sistema selecionará o maior digitado");
         printf("\n%d)Digite um numero: ", cont);
         scanf("%d",&num);
         peso=(num>peso?num:peso);
